fde2ae67e54ab6f3f319372b08d981fcef7b8767
leonardo <leordr.dev@gmail.com>
2025-11-27 19:46:18 -0300

    feat: add area util

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@ import './global.css';
 import { StatusBar } from 'expo-status-bar';
 
 import { LoginScreen } from '@/screens/login';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <>
-      
-      <LoginScreen />
+      <SafeAreaView className="flex-1">
+        <LoginScreen />
+      </SafeAreaView>
+
       <StatusBar style="auto" />
     </>
   );
 }
-
