666149424bf197f4411067776f360b000edc310b
leonardo <leordr.dev@gmail.com>
2025-12-05 20:03:45 -0300

    feat: add lista

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 
 import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Soma } from '@/screens/soma';
+import { ListaScreen } from '@/screens/listas';
 
 export default function App() {
   return (
     <>
       <SafeAreaView className="flex-1">
-        <LoginScreen />
+        <ListaScreen />
       </SafeAreaView>
 
       <StatusBar style="auto" />
