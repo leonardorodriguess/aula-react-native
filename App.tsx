22b91df79767d77914f2d2e63af20e7f6bb68d51
leonardo <leordr.dev@gmail.com>
2025-12-18 18:40:09 -0300

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -5,12 +5,21 @@ import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Soma } from '@/screens/soma';
 import { ListaScreen } from '@/screens/listas';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <>
       <SafeAreaView className="flex-1">
-        <ListaScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="login" component={LoginScreen} />
+            <Stack.Screen name="soma" component={Soma} />
+            <Stack.Screen name="listas" component={ListaScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
 
       <StatusBar style="auto" />
